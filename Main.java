ab27e6ae359d53bb2159ee4fb2b43338c40d7226
MateuszPisula01 <126687627+MateuszPisula01@users.noreply.github.com>
2023-03-15 14:23:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+
+    int F;
+
+    for( int C=0;C<=300;C+=20)
+      {  F=((C*9/5)+32);
+        System.out.println(C + " Stopni C to "+ F + " Stopni F " );
+      
+        
+      }
+  }
+}
